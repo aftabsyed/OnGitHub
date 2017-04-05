a1746602673d4daa6540845f5cd4e6765ba6141d
aftabsyed <email2aftab@gmail.com>
2017-04-05 16:41:54 +1000

    test varaible and output

---
+++ ConsoleOnGitHub/ConsoleOnGitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleOnGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = "abc";
+            Console.WriteLine("Simple value is" + test);
+        }
+    }
+}
